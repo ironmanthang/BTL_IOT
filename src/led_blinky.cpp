d128d758ca3a4a84ab9d202cddf8bfd6b2426187
nghiem2705 <nghiemgithub@gmail.com>
2026-04-15 00:48:24 +0700

    feat(led): xong task 1, bắt semaphore chớp led 3 nấc nhiệt độ

--- src/led_blinky.cpp
+++ src/led_blinky.cpp
@@ -1,12 +1,26 @@
 #include "led_blinky.h"
 
-void led_blinky(void *pvParameters){
-  pinMode(LED_GPIO, OUTPUT);
-  
-  while(1) {                        
-    digitalWrite(LED_GPIO, HIGH);  // turn the LED ON
-    vTaskDelay(1000);
-    digitalWrite(LED_GPIO, LOW);  // turn the LED OFF
-    vTaskDelay(1000);
-  }
+void led_blinky(void *pvParameters) {
+    pinMode(LED_GPIO, OUTPUT);
+    DisplayState_t current_state = STATE_NORMAL;
+    SensorData_t data;
+
+    while(1) {
+        if (xSemaphoreTake(xSemaphoreStateChange, 0) == pdTRUE) {
+            if (sensorData_read(&data)) {
+                current_state = data.state;
+            }
+        }
+
+        if (current_state == STATE_NORMAL) {
+            digitalWrite(LED_GPIO, LOW);
+            vTaskDelay(pdMS_TO_TICKS(500));
+        } else if (current_state == STATE_WARNING) {
+            digitalWrite(LED_GPIO, !digitalRead(LED_GPIO));
+            vTaskDelay(pdMS_TO_TICKS(1000)); 
+        } else if (current_state == STATE_CRITICAL) {
+            digitalWrite(LED_GPIO, !digitalRead(LED_GPIO));
+            vTaskDelay(pdMS_TO_TICKS(200)); 
+        }
+    }
 }
